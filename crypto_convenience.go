1c1838debebd5cc3785d5a9752de027a3334c63d
leobrada <leo.bradatsch@gmail.com>
2022-04-04 05:08:31 +0000

    rsa functions added

--- crypto_convenience.go
+++ crypto_convenience.go
@@ -4,8 +4,11 @@ import (
     "fmt"
     "crypto/tls"
     "crypto/x509"
+    "crypto/rsa"
     "io/ioutil"
 )
+
+// X509 PART
 
 // function unifies the loading of CA certificates
 func LoadCACertificate(certfile string, certPool *x509.CertPool) error {
@@ -26,4 +29,63 @@ func LoadX509KeyPair(certfile, keyfile string) (tls.Certificate, error) {
     }
 
     return keyPair, nil
+}
+
+// RSA PART
+
+// ParseRsaPublicKeyFromPemFile() loads rsa.PublicKey from a PEM file
+func ParseRsaPublicKeyFromPemFile(pubPEMLocation string) (*rsa.PublicKey, error) {
+        // Read a file content and convert it to a PEM block
+        pemBlock, err := readFirstPEMBlockFromFile(pubPEMLocation)
+        if err != nil {
+                return nil, fmt.Errorf("ParseRsaPublicKeyFromPemFile(): %w", err)
+        }
+
+        if !strings.Contains(pemBlock.Type, "PUBLIC KEY") {
+                fmt.Printf("pemBlock.Type = %#v\n", pemBlock.Type)
+                return nil, errors.New("ParseRsaPublicKeyFromPemFile(): provided file does not contain a PEM public key")
+        }
+
+        pub, err := x509.ParsePKIXPublicKey(pemBlock.Bytes)
+        if err == nil {
+                return pub.(*rsa.PublicKey), nil
+        }
+
+        pub, err = x509.ParsePKCS1PublicKey(pemBlock.Bytes)
+        if err == nil {
+                return pub.(*rsa.PublicKey), nil
+        }
+
+        // Another Public keys form parsing functions can be added here later
+        // ...
+
+        return nil, fmt.Errorf("ParseRsaPublicKeyFromPemFile(): unable to parse JWT public key: %w", err)
+}
+
+// ParseRsaPrivateKeyFromPemFile() loads rsa.PrivateKey from a PEM file
+func ParseRsaPrivateKeyFromPemFile(privPEMLocation string) (*rsa.PrivateKey, error) {
+        // Read a file content and convert it to a PEM block
+        pemBlock, err := readFirstPEMBlockFromFile(privPEMLocation)
+        if err != nil {
+                return nil, fmt.Errorf("ParseRsaPrivateKeyFromPemFile(): %w", err)
+        }
+
+        if !strings.Contains(pemBlock.Type, "PRIVATE KEY") {
+                return nil, errors.New("ParseRsaPrivateKeyFromPemFile(): provided file does not contain a PEM private key")
+        }
+
+        priv, err := x509.ParsePKCS8PrivateKey(pemBlock.Bytes)
+        if err == nil {
+                return priv.(*rsa.PrivateKey), nil
+        }
+
+        priv, err = x509.ParsePKCS1PrivateKey(pemBlock.Bytes)
+        if err == nil {
+                return priv.(*rsa.PrivateKey), nil
+        }
+
+        // Another Private keys form parsing functions can be added here later
+        // ...
+
+        return nil, fmt.Errorf("ParseRsaPrivateKeyFromPemFile(): unable to parse JWT private key: %w", err)
 }
